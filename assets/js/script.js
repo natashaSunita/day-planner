a667bf5d06325fa8874b3a3c970f291a36129a03
Natasha Beddow <beddow90@gmail.com>
2022-05-10 17:01:48 +0100

    added conditions to colourcode function and styled textareas

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,31 @@ currentDate.textContent = moment().format("Do MMM YYYY");
 currentTime.textContent = moment().format("k:mm:ss a");
 
 const thing = hourBlocks[0].getAttribute("id");
-console.log(thing.slice(5));
+console.log(hourBlocks[0].children[1]);
 
 function colourCode() {
     let currentHour = moment().format("HH");
     for (let i = 0; i < hourBlocks.length; i++) {
         let h = hourBlocks[i].getAttribute("id").slice(5);
-    }
-    if (h < currentHour) {
-        
+        if (h < currentHour) {
+            // change background colour of textarea to grey
+            // add class to textarea of hourblocks
+            hourBlocks[i].children[1].classList.add("past");
+        } else if (h == currentHour) {
+            hourBlocks[i].children[1].classList.add("present");
+        } else if (h > currentHour) {
+            hourBlocks[i].children[1].classList.add("future");
+        }
     }
 }
 
 colourCode();
-// 
 
 
 
-// shape of i, thirsty, what am I doing?
+
+// shape of i, thirsty, what am I doing? websites and their layers
+// whtat would I like to be really good at? drawing? music? biology? gardening?
 
 // target by class
 // if time is < then current time
