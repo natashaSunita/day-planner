8f3632d4e2430280238af9829215c214fbcd4f34
Natasha Beddow <beddow90@gmail.com>
2022-05-10 13:26:18 +0100

    added hour-text class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 const currentDate = document.getElementById("current-date");
 const currentTime = document.getElementById("current-time");
+const hourBlocks = document.querySelectorAll(".hour-text");
 
 currentDate.textContent = moment().format("Do MMM YYYY");
 currentTime.textContent = moment().format("k:mm:ss a");
+
+// document.getElementById("hour1-text").classList.add("past");
+
+console.log(hourBlocks);
+
+
+
+
+// target by class
+// if time is < then current time
+// color grey
+// if time is equal to current time
+// color green
+// if time is > than current time
+// color pink
