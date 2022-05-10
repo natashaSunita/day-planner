33bde2b491fee788bb5b99e36bb72b4276feda71
Natasha Beddow <beddow90@gmail.com>
2022-05-10 16:13:09 +0100

    started creating colourcode function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
 const currentDate = document.getElementById("current-date");
 const currentTime = document.getElementById("current-time");
-const hourBlocks = document.querySelectorAll(".hour-text");
+const hourBlocks = document.querySelectorAll(".rows");
 
 currentDate.textContent = moment().format("Do MMM YYYY");
 currentTime.textContent = moment().format("k:mm:ss a");
 
-// document.getElementById("hour1-text").classList.add("past");
+const thing = hourBlocks[0].getAttribute("id");
+console.log(thing.slice(5));
 
-console.log(hourBlocks);
+function colourCode() {
+    let currentHour = moment().format("HH");
+    for (let i = 0; i < hourBlocks.length; i++) {
+        let h = hourBlocks[i].getAttribute("id").slice(5);
+    }
+    if (h < currentHour) {
+        
+    }
+}
+
+colourCode();
+// 
 
 
 
+// shape of i, thirsty, what am I doing?
 
 // target by class
 // if time is < then current time
